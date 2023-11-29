cb0a9fb6ff1eecb25f1542c98e0602c43bc22e51
Santiago Noh <santiagocahum25@gmail.com>
2023-11-28 22:37:57 -0600

    actualizacion bd

--- db_tutorias.sql
+++ db_tutorias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3308
--- Tiempo de generación: 26-11-2023 a las 07:20:16
+-- Tiempo de generación: 29-11-2023 a las 04:35:40
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.4.9
 
@@ -136,7 +136,8 @@ CREATE TABLE IF NOT EXISTS `auth_assignment` (
 --
 
 INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
-('Admin', '9', 1694442604);
+('admin', '9', 1694442604),
+('tutor', '11', 1701109434);
 
 -- --------------------------------------------------------
 
@@ -164,8 +165,26 @@ CREATE TABLE IF NOT EXISTS `auth_item` (
 
 INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
 ('/*', 2, NULL, NULL, NULL, 1694441822, 1694441822),
-('Admin', 1, 'Administrador con con todos los privilegios', NULL, NULL, 1694442495, 1694442495),
-('All', 2, 'Acceso completo al sistema', NULL, NULL, 1694442207, 1694442207);
+('/ciclo-escolar/create', 2, NULL, NULL, NULL, 1701109223, 1701109223),
+('/ciclo-escolar/delete', 2, NULL, NULL, NULL, 1701109223, 1701109223),
+('/ciclo-escolar/index', 2, NULL, NULL, NULL, 1701109223, 1701109223),
+('/ciclo-escolar/update', 2, NULL, NULL, NULL, 1701109223, 1701109223),
+('/ciclo-escolar/view', 2, NULL, NULL, NULL, 1701109223, 1701109223),
+('/gii/*', 2, NULL, NULL, NULL, 1701123881, 1701123881),
+('/pat/index', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/semana-real/create', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/semana-real/delete', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/semana-real/update', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/semana/detail-add-semana-real', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/semana/view', 2, NULL, NULL, NULL, 1701132157, 1701132157),
+('/semana/view-add-semana', 2, NULL, NULL, NULL, 1701131244, 1701131244),
+('/tutor/index', 2, NULL, NULL, NULL, 1701124042, 1701124042),
+('/tutor/update', 2, NULL, NULL, NULL, 1701124042, 1701124042),
+('/tutor/view', 2, NULL, NULL, NULL, 1701124042, 1701124042),
+('admin', 1, 'Administrador con con todos los privilegios', NULL, NULL, 1694442495, 1694442495),
+('All', 2, 'Acceso completo al sistema', NULL, NULL, 1694442207, 1694442207),
+('PTutor', 2, 'Permiso para el tutor', NULL, NULL, 1701109263, 1701109263),
+('tutor', 1, 'Rol para los tutores registrados en el sistema', NULL, NULL, 1701109342, 1701109342);
 
 -- --------------------------------------------------------
 
@@ -187,7 +206,19 @@ CREATE TABLE IF NOT EXISTS `auth_item_child` (
 
 INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
 ('All', '/*'),
-('Admin', 'All');
+('PTutor', '/gii/*'),
+('PTutor', '/pat/index'),
+('PTutor', '/semana-real/create'),
+('PTutor', '/semana-real/delete'),
+('PTutor', '/semana-real/update'),
+('PTutor', '/semana/detail-add-semana-real'),
+('PTutor', '/semana/view'),
+('PTutor', '/semana/view-add-semana'),
+('PTutor', '/tutor/index'),
+('PTutor', '/tutor/update'),
+('PTutor', '/tutor/view'),
+('admin', 'All'),
+('tutor', 'PTutor');
 
 -- --------------------------------------------------------
 
@@ -366,6 +397,31 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pat`
+--
+
+DROP TABLE IF EXISTS `pat`;
+CREATE TABLE IF NOT EXISTS `pat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_semestre` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text,
+  `estatus` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_semestre` (`id_semestre`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pat`
+--
+
+INSERT INTO `pat` (`id`, `id_semestre`, `nombre`, `descripcion`, `estatus`) VALUES
+(1, 1, 'PAT PRIMER SEMESTRE', 'V1 PAT PARA LOS PRIMEROS SEMESTRES DE TODAS LAS INGENIERIAS', 1),
+(3, 1, 'PAT PRIMER SEMESTRE V3', '', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `periodo_escolar`
 --
 
@@ -416,6 +472,63 @@ INSERT INTO `rol` (`id`, `rol_nombre`, `rol_valor`) VALUES
 (2, 'Admin', 100),
 (4, 'Capturista', 20),
 (10, 'Coordinador', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `semana`
+--
+
+DROP TABLE IF EXISTS `semana`;
+CREATE TABLE IF NOT EXISTS `semana` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `num_semana` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo_tutoria` int(11) NOT NULL,
+  `tematica` text NOT NULL,
+  `objetivos` text NOT NULL,
+  `justificacion` text NOT NULL,
+  `estrategias_tutor` text NOT NULL,
+  `acciones` text NOT NULL,
+  `estrategias_tutorado` text,
+  `id_pat` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_pat` (`id_pat`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `semana`
+--
+
+INSERT INTO `semana` (`id`, `num_semana`, `nombre`, `tipo_tutoria`, `tematica`, `objetivos`, `justificacion`, `estrategias_tutor`, `acciones`, `estrategias_tutorado`, `id_pat`) VALUES
+(1, 1, 'SEMANA 1', 0, '<p>Bienvenida, presentación por parte del Tutor, Actualización del Expediente.&nbsp;</p><p><font style=\"background-color: rgb(255, 255, 0);\" color=\"#ff0000\"><b>SESIÓN PRESENCIAL CON EL TUTOR PASAR LISTA</b></font></p>', '<p>El docente contactará y se organiganizará con los alumnos para poder brindarles instrucciones&nbsp; de la actualización del Expediente y proporcionarles información relevante a traves del jefe de grupo o por medio de una aplicación web, whatsapp, meet etc..&nbsp;<br></p>', '<p>Es de suma importancia que los alumnos se sientan acompañados a través de su ciclo escolar ya que por eso se les asigna un tutor por grupo y asi los tutores juegan un papel importante como guias, ya que un tutor es necesario en el crecimiento del perfil estudiantil. El tutor tiene la obligación de estar pendiente de cada alumno y transmitirle motivación de continuar día a día cosntruyendo juntos un futuro agradable.<br></p>', '<p>Contactarse con el&nbsp; grupo del salón ó de preferencia con todo el grupo de manera presencial&nbsp; ó a través de una herramienta de redes sociales, para poder brindarle información. Integrar al grupo de una manera amable y coordial.</p><p>Proporcionar Información relevante a los alumnos.PASAR LISTA Y CORROBORAR CON SUS MAESTROS SI ALGUN ALUMNO ESTA FALTANDO O NO SE ESTA CONECTANDO</p>', '<p>El docente se presentó o conectó con el grupo para darles la instrucción y ellos puedan realizar la actualización del expediente, posteriormente se contacto con el jefe de grupo correspondiente para poder enviarle la informarle de noticias relevantes acerca del programa de tutorias, del cual&nbsp;el jefe de grupo se los compartio a sus compañeros de tal modo se estuvo monitoriando con cada uno para poder tener su confirmación de recibido y actualización de su expediente.<br></p>', '<ul><li>Responder en tiempo y en forma para su confirmación de haberse enterado de la información dadá por el tutor&nbsp; a través de un medio de Tecnología.</li><li>Participar&nbsp; y Estar puntual cuando los (as) citen. En Linea y actualizar su expediente</li><li>Participación</li></ul>', 1),
+(2, 2, 'SEMANA 2', 0, '<p>Actualización del Expediente.&nbsp;</p><p><font style=\"background-color: rgb(255, 255, 0);\" color=\"#ff0000\"><b>SESIÓN PRESENCIAL CON EL TUTOR PASAR LISTA</b></font></p>', '<p>El docente asigando como tutor se&nbsp; presentará ó pondrá en contacto a través de una aplicación web con su grupo correspondiente para darle seguimiento a la actualización del expediente, motivarlos y brindarles información neesaria del perfil del tutor&nbsp; &nbsp;&nbsp;<br></p>', '<p>Contar con información actualizada del estudiante, a través de los formularios en línea y a su vez poder contactarlos cuando se requiera por los departamentos de psicología, trayectoría académica, la coordinación o sus tutores.</p><p>Cabe mencionar que es obligatorio que el alumno llene su expediente de manera correcta y completa, de tal forma que si no lo hace, no será liberado del semestre de manera automática.</p>', '<p>Darle seguimiento y que los alumnos actualicen sus expedientes, Brindarle información necesaria del perfil del tutor. PASAR LISTA Y CORROBORAR CON SUS MAESTROS SI ALGUN ALUMNO ESTA FALTANDO O NO SE ESTA CONECTANDO<br></p>', '<p>El docente se presentará&nbsp; en su sesión, de manera f´´isica o virtual&nbsp; a través de una Video Conferencia por medio de la aplicación google meet&nbsp; para transmitir y explicar&nbsp; a los alumnos como realizará la actualización del expediente en la página del Tecnológico Nacional de México Campus Valladolid, los pasos son los siguientes</p><p>Entran a la página principal del ITSVA,&nbsp; en la pestaña estudiantes,&nbsp; pestaña tutoría y&nbsp; actualizar expediente en el link, tienen que tener su correo institucional y su clave es muy importante.&nbsp; Se realizó una captura de pantalla para tener evidencia de la tutoría.&nbsp; &nbsp;</p>', '<p>SE PASARÁN LAS DIAPOSTIVAS FORMADOR DE FORMADORES Y REALIZARAN LOS ALUMNOS EL EJERCICIO, EL MATERIAL ESTA EL EL DRIVE&nbsp;<br></p>', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `semana_real`
+--
+
+DROP TABLE IF EXISTS `semana_real`;
+CREATE TABLE IF NOT EXISTS `semana_real` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_grupomaster` int(11) NOT NULL,
+  `id_semana` int(11) NOT NULL,
+  `semana_atendida` int(11) NOT NULL,
+  `alumnos_atendidos` int(11) NOT NULL,
+  `alumnos_faltantes` int(11) NOT NULL,
+  `total_alumnos` int(11) NOT NULL,
+  `atendidos_hombres` int(11) NOT NULL,
+  `atendidos_mujeres` int(11) NOT NULL,
+  `total_gatendidos` int(11) NOT NULL,
+  `evidencias` longtext NOT NULL,
+  `observaciones` text NOT NULL,
+  `alumnos` text,
+  PRIMARY KEY (`id`),
+  KEY `idx_grupomaster` (`id_grupomaster`),
+  KEY `idx_semana` (`id_semana`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -525,8 +638,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(9, 'root', 'El4u2OFmro6HdVohznDGhv45RhYiBoe8', '$2y$13$b3MlkEF5WmnTFIfdTzyTV.XTSCttIOBcKInVnAp9GlkDPZ0hFas0C', NULL, 'rusell.im@valladolid.tecnm.mx', 10, 1653351873, 1653351873, 'QnphEMrAbjb3aM5EGodGe7pCW1PPuhsl_1653351873'),
-(11, 'luis.nc', 'm3XEmH6t_-_cyYRFoZywJ_rNP5IF1-rI', '$2y$13$TY5V6KQV1IkBBYd5ldZPNeqlmxC76/QPZR0KrVAEwjLqObF2txBjG', NULL, 'l19070049@valladolid.tecnm.mx', 9, 1700430159, 1700452464, 'Iv7z2nBrWJkL65PR8Ec6O6uO7_ryGXd6_1700430159');
+(9, 'root', 'El4u2OFmro6HdVohznDGhv45RhYiBoe8', '$2y$13$b3MlkEF5WmnTFIfdTzyTV.XTSCttIOBcKInVnAp9GlkDPZ0hFas0C', NULL, 'rusell.im@valladolid.tecnm.mx', 10, 1653351873, 1701108543, 'QnphEMrAbjb3aM5EGodGe7pCW1PPuhsl_1653351873'),
+(11, 'luis.nc', 'm3XEmH6t_-_cyYRFoZywJ_rNP5IF1-rI', '$2y$13$TY5V6KQV1IkBBYd5ldZPNeqlmxC76/QPZR0KrVAEwjLqObF2txBjG', NULL, 'l19070049@valladolid.tecnm.mx', 10, 1700430159, 1700452464, 'Iv7z2nBrWJkL65PR8Ec6O6uO7_ryGXd6_1700430159');
 
 --
 -- Restricciones para tablas volcadas
@@ -574,11 +687,30 @@ ALTER TABLE `grupo_master`
   ADD CONSTRAINT `fk_IdTutor` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id`) ON UPDATE CASCADE;
 
 --
+-- Filtros para la tabla `pat`
+--
+ALTER TABLE `pat`
+  ADD CONSTRAINT `fk_idSemestrePAT` FOREIGN KEY (`id_semestre`) REFERENCES `semestre` (`id`) ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `periodo_escolar`
 --
 ALTER TABLE `periodo_escolar`
   ADD CONSTRAINT `fk_idCiclo` FOREIGN KEY (`id_ciclo`) REFERENCES `ciclo_escolar` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_idEstatus` FOREIGN KEY (`id_estatus`) REFERENCES `estatus` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `semana`
+--
+ALTER TABLE `semana`
+  ADD CONSTRAINT `fk_idPat_semana` FOREIGN KEY (`id_pat`) REFERENCES `pat` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `semana_real`
+--
+ALTER TABLE `semana_real`
+  ADD CONSTRAINT `fk_idGrupoMaster_Real` FOREIGN KEY (`id_grupomaster`) REFERENCES `grupo_master` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idSemana_SemanaReal` FOREIGN KEY (`id_semana`) REFERENCES `semana` (`id`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `tutor`
